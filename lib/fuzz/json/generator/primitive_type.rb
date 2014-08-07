dca87f32c388e0e8b1ab10479d9a294a770bc5b2
Naoki Shimizu <naoki.shimizu@dena.com>
2014-08-07 09:45:35 +0900

    clean up

--- lib/fuzz/json/generator/primitive_type.rb
+++ lib/fuzz/json/generator/primitive_type.rb
@@ -1,1 +1,11 @@
 Dir[File.join(File.dirname(__FILE__), "primitive_type/*.rb")].each {|file| require file }
+
+module Fuzz
+  module JSON
+    class Generator
+      class PrimitiveType
+      end
+    end
+  end
+end
+
